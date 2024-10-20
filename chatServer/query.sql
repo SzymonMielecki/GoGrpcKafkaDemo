cf77c752a7dd43e664ed640a7d48ad2ad06d4c13
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2024-10-20 16:33:46 +0200

    cases

--- chatServer/query.sql
+++ chatServer/query.sql
@@ -1,2 +1,2 @@
 -- name: CreateMessage :exec
-INSERT INTO messages (Content, SenderId) VALUES ($1, $2);
+INSERT INTO Messages (Content, SenderId) VALUES ($1, $2);
--- chatServer/schema.sql
+++ chatServer/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE messages (
+CREATE TABLE Messages (
     Id INT PRIMARY KEY,
     Content TEXT NOT NULL,
     SenderId INT
